160958fda53cd437a79da022cadbf1f7355c473e
cpauvert <cpauvert@users.noreply.github.com>
2023-11-03 13:43:13 +0100

    restore full workflow but error in setting reference

--- _targets.R
+++ _targets.R
@@ -74,89 +74,89 @@ list(
     # Named lists are problematic for dynamic branching
     # as the name are appended to the matrix rownames
     merge_processed_spectra(unname(processed))
-  )#,
-  # tar_target(
-  #   sim_interpolated,
-  #   coop::tcosine(fm_interpolated)
-  # ),
-  # tar_target(
-  #   excel_metadata,
-  #   which_plate_metadata,
-  #   format = "file"
-  # ),
-  # tar_target(# Get metadata from excel sheet)
-  #   metadata,
-  #   read_excel(excel_metadata) %>%
-  #     select(-c("Well Selected_MALDI_hits"))
-  # ),
-  # tar_target(
-  #   metadata_picking,
-  #   metadata %>% rename(
-  #     c("OD600" = "Well OD600_BlankCorrected_MALDI_Step2_2Tag",
-  #       "name" = "Well SampleName")) %>%
-  #     dplyr::mutate(
-  #       well = gsub(".*_([0-9]{1,3}$)", "\\1", name) %>%
-  #         strtoi(),
-  #       is_edge = maldipickr::is_well_on_edge(
-  #         well_number = well, plate_layout = 384
-  #       )) %>%
-  #     select(name, OD600, is_edge)
-  # ),
-  # tar_target(
-  #   df_interpolated,
-  #   delineate_with_similarity(sim_interpolated, threshold = which_threshold, method = "complete")
-  # ),
-  # tar_target(
-  #   clusters,
-  #   set_reference_spectra(df_interpolated, processed$metadata)
-  # ),
-  # tar_target(#clean up spectra names for cluster name
-  #   # remove trailing _B11
-  #   clusters_clean,
-  #   clusters %>% dplyr::mutate(
-  #     name = gsub("_[A-Z][0-9]{1,3}$","",name)
-  #   )
-  # ),
-  # tar_target(
-  #   # subset metadata information
-  #   metadata_subset,
-  #   metadata_picking %>% semi_join(clusters_clean, by = "name")
-  # ),
-  # tar_target(
-  #   picked,
-  #   pick_spectra(
-  #     cluster_df = clusters_clean,
-  #     metadata_df = metadata_subset,
-  #     criteria_column = "OD600",
-  #     soft_mask_column = "is_edge")),
-  # tar_target(
-  #   summary_picked,
-  #   picked %>% filter(to_pick) %>%
-  #     transmute(
-  #       name = name,
-  #       cluster_size = cluster_size,
-  #       procedure = paste("Strejcek", which_threshold, sep = "_")
-  #     )
-  # ),
-  # tar_target(# prep excel sheet
-  #   prep_excel,
-  #   metadata %>% 
-  #     left_join(
-  #       picked %>% transmute(
-  #         `Well SampleName` = name,
-  #         `Well Selected_MALDI_hits` = as.integer(to_pick)
-  #       ), by = "Well SampleName"
-  #     ) %>%
-  #     mutate(
-  #       `Well Selected_MALDI_hits` = replace_na(`Well Selected_MALDI_hits`, 0)
-  #     )
-  # ),
-  # tar_target(
-  #   excel_output,
-  #   write_xlsx(prep_excel,
-  #              path = paste0(
-  #                "picked_",paste("Strejcek", which_threshold, sep = "_"),
-  #                "_",basename(which_plate_metadata))
-  #   )
-  # )
+  ),
+  tar_target(
+    sim_interpolated,
+    coop::tcosine(fm_interpolated)
+  ),
+  tar_target(
+    excel_metadata,
+    which_plate_metadata,
+    format = "file"
+  ),
+  tar_target(# Get metadata from excel sheet)
+    metadata,
+    read_excel(excel_metadata) %>%
+      select(-c("Well Selected_MALDI_hits"))
+  ),
+  tar_target(
+    metadata_picking,
+    metadata %>% rename(
+      c("OD600" = "Well OD600_BlankCorrected_MALDI_Step2_2Tag",
+        "name" = "Well SampleName")) %>%
+      dplyr::mutate(
+        well = gsub(".*_([0-9]{1,3}$)", "\\1", name) %>%
+          strtoi(),
+        is_edge = maldipickr::is_well_on_edge(
+          well_number = well, plate_layout = 384
+        )) %>%
+      select(name, OD600, is_edge)
+  ),
+  tar_target(
+    df_interpolated,
+    delineate_with_similarity(sim_interpolated, threshold = which_threshold, method = "complete")
+  ),
+  tar_target(
+    clusters,
+    set_reference_spectra(df_interpolated, processed$metadata)
+  ),
+  tar_target(#clean up spectra names for cluster name
+    # remove trailing _B11
+    clusters_clean,
+    clusters %>% dplyr::mutate(
+      name = gsub("_[A-Z][0-9]{1,3}$","",name)
+    )
+  ),
+  tar_target(
+    # subset metadata information
+    metadata_subset,
+    metadata_picking %>% semi_join(clusters_clean, by = "name")
+  ),
+  tar_target(
+    picked,
+    pick_spectra(
+      cluster_df = clusters_clean,
+      metadata_df = metadata_subset,
+      criteria_column = "OD600",
+      soft_mask_column = "is_edge")),
+  tar_target(
+    summary_picked,
+    picked %>% filter(to_pick) %>%
+      transmute(
+        name = name,
+        cluster_size = cluster_size,
+        procedure = paste("Strejcek", which_threshold, sep = "_")
+      )
+  ),
+  tar_target(# prep excel sheet
+    prep_excel,
+    metadata %>%
+      left_join(
+        picked %>% transmute(
+          `Well SampleName` = name,
+          `Well Selected_MALDI_hits` = as.integer(to_pick)
+        ), by = "Well SampleName"
+      ) %>%
+      mutate(
+        `Well Selected_MALDI_hits` = replace_na(`Well Selected_MALDI_hits`, 0)
+      )
+  ),
+  tar_target(
+    excel_output,
+    write_xlsx(prep_excel,
+               path = paste0(
+                 "picked_",paste("Strejcek", which_threshold, sep = "_"),
+                 "_",basename(which_plate_metadata))
+    )
+  )
 )
