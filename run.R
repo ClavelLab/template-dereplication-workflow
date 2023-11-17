c4deb4222048c3c0a25e252bef04f4a23de79e10
cpauvert <cpauvert@users.noreply.github.com>
2023-11-17 11:16:01 +0100

    set up a dedicated workflow for fast growers firs

--- run.R
+++ run.R
@@ -5,6 +5,9 @@
 # See https://books.ropensci.org/targets/hpc.html
 # to learn about your options.
 
+# Run the first part of the workflow
+# https://books.ropensci.org/targets/projects.html#run-each-project
+Sys.setenv(TAR_PROJECT = "fast_growers")
 targets::tar_make()
 # targets::tar_make_clustermq(workers = 2) # nolint
 # targets::tar_make_future(workers = 2) # nolint
